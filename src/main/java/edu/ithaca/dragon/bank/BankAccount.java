bddff8e944139a6a78b1af085b600a2fd9275101
Peter Mankiewich <pmank64@gmail.com>
2019-01-25 09:16:32 -0500

    Comments Added

--- src/main/java/edu/ithaca/dragon/bank/BankAccount.java
+++ src/main/java/edu/ithaca/dragon/bank/BankAccount.java
@@ -34,7 +34,10 @@ public class BankAccount {
 
     }
 
-
+    //static means you can call the method without the object, or without instantiating
+    //can't refer to object, which means you can't use email and balance instance variables in this method
+    //unit testing allows us to see that this specific method is working, and that it is not a problem with an object
+    //this is a pure method because it does not change anything
     public static boolean isEmailValid(String email){
         if (email.indexOf('@') == -1){
             return false;
--- src/test/java/edu/ithaca/dragon/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dragon/bank/BankAccountTest.java
@@ -21,12 +21,16 @@ class BankAccountTest {
         assertEquals(100, bankAccount.getBalance());
     }
 
+    //create a javadoc to define a valid email
+    //write more tests, some will pass and other will fail
+    //then commit
     @Test
     void isEmailValidTest(){
         assertTrue(BankAccount.isEmailValid( "a@b.com"));
         assertFalse( BankAccount.isEmailValid(""));
     }
 
+    //the constructor relies on other methods and code so it is not really a perfect unit test
     @Test
     void constructorTest() {
         BankAccount bankAccount = new BankAccount("a@b.com", 200);
